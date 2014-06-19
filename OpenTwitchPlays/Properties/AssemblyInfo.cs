7a408dceb85e05c9a5fbe64952b22e90d7dc32a7
fuyangli <guaitolinigabriel@gmail.com>
2014-06-19 18:09:55 -0300

    Assembly Versioning
    Assembly Versioning
    Good to track application version

--- OpenTwitchPlays/Properties/AssemblyInfo.cs
+++ OpenTwitchPlays/Properties/AssemblyInfo.cs
@@ -24,13 +24,21 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+//      YY - Year
+//      MM - Month
+//      DD - Day
+//      RR - Revision of the day
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Historic Version
+// YY.MM.DD.RR  -   Programmer  -   Alterations
+//                                  Multiline Comment
+// [assembly: AssemblyVersion("YY.MM.DD.RR")]
+[assembly: AssemblyVersion("14.06.19.01")]
+[assembly: AssemblyFileVersion("14.06.19.01")]
+
+// Historic Version
+// 14.06.19.01  -   Gabriel Guaitolini [Fuyangli]   -   Added Assembly Versioning
+
+
+
+
